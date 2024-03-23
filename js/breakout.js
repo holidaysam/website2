8800a4c66d9a5d68505e58c9fa77c23354aabaa8
Sam Holiday <142548008+holidaysam@users.noreply.github.com>
2024-03-23 05:15:50 +0000

    Fri, Mar 22, 2024, 10:15 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -165,6 +165,20 @@ function moveBall() {
     ) {
         ball.dy = -1 * ball.dy
     }
+
+    // brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if (brick.visible) {
+                if (
+                    ball.y - ball.size < brick.y + brick.h // bottom
+                ) {
+                    ball.dy = -1 * ball.dy
+                    brick.visible = false
+                }
+            }
+        })
+    })
 }
 
 // update canvas drawing and animation
