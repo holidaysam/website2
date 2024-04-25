13401c6762b211d0ca4f97487497e95bd464ef1d
Sam Holiday <142548008+holidaysam@users.noreply.github.com>
2024-04-25 20:01:56 +0000

    Thu, Apr 25, 2024, 1:01 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,5 +1,5 @@
 const screens = document.querySelectorAll('.screen')
-const choose_insect_btns = document.querySelectorAll('choose-insect-btn')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
 const game_container = document.getElementById('game-container')
 const start_btn = document.getElementById('start-btn')
 const timeEl = document.getElementById('time')
@@ -15,27 +15,19 @@ start_btn.addEventListener('click', () => {
     screens[0].classList.add('up')
 })
 
-choose_insect_btns[0].addEventListener('click', () => {
-    console.log("H")
-})
 
 choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        console.log("HELLO")
+        console.log("CLICK")
+        const img = btn.querySelector('img')
+        const alt = img.getAttribute('alt')
+        const src = img.getAttribute('src')
+        screens[1].classList.add('up')
+        selected_insect = {src, alt}
+        setTimeout(createInsect, 1000)
+        startGame()
     })
 })
-// choose_insect_btns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//         console.log("CLICK")
-//         const img = btn.querySelector('img')
-//         const alt = img.getAttribute('alt')
-//         const src = img.getAttribute('src')
-//         screens[1].classList.add('up')
-//         selected_insect = {src, alt}
-//         setTimeout(createInsect, 1000)
-//         startGame()
-//     })
-// })
 
 function createInsect() {
     const insect = document.createElement('div')
