b8d400db54755e59118c8422063d8d045c78833f
Sam Holiday <142548008+holidaysam@users.noreply.github.com>
2024-04-25 19:56:02 +0000

    Thu, Apr 25, 2024, 12:56 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -17,6 +17,7 @@ start_btn.addEventListener('click', () => {
 
 choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', () => {
+        console.log("CLICK")
         const img = btn.querySelector('img')
         const alt = img.getAttribute('alt')
         const src = img.getAttribute('src')
